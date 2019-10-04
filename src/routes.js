49f6686188302e6c75b55d36a108b03e34e7d6a3
Zakariaa Oulhafiane <zakariaa.oulhafiane@gmail.com>
2019-10-04 10:22:04 +0100

    First Commit (Login & Register pages)

---
+++ src/routes.js
@@ -0,0 +1,20 @@
+import Register from "views/Register.jsx";
+import Login from "views/Login.jsx";
+
+var routes = [
+  {
+    path: "/login",
+    name: "Login",
+    icon: "ni ni-key-25 text-info",
+    component: Login,
+    layout: "/auth"
+  },
+  {
+    path: "/register",
+    name: "Register",
+    icon: "ni ni-circle-08 text-pink",
+    component: Register,
+    layout: "/auth"
+  }
+];
+export default routes;
