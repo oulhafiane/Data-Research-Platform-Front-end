f600001b9cfd7155c18e0e54c178e89c2a77b32f
Zakariaa Oulhafiane <zakariaa.oulhafiane@gmail.com>
2019-10-15 17:28:33 +0100

    Link scroll fixed

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const DEFAULT_URL = "http://localhost:8000/";
+export const DEFAULT_URL =
+  "http://Drp-env.ik39jx2tyf.eu-west-3.elasticbeanstalk.com/";
--- src/index.js
+++ src/index.js
@@ -12,9 +12,11 @@ import "assets/scss/argon-dashboard-react.scss";
 import "assets/css/shards-dashboards.1.1.0.min.css";
 import "assets/scss/argon-design-system-react.scss";
 import "assets/vendor/font-awesome/css/font-awesome.min.css";
+import ScrollToTop from "components/Scroll/ScrollUp";
 
 ReactDOM.render(
   <BrowserRouter>
+    <ScrollToTop />
     <Switch>
       <Route path="/auth" render={props => <AuthLayout {...props} />} />
       <Route path="/default" render={props => <Default {...props} />} />
