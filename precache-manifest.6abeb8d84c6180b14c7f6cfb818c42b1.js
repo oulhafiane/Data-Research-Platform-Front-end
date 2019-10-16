861823d40047d6984ccdb885b6971eb4c70c3be1
Zakariaa Oulhafiane <zakariaa.oulhafiane@gmail.com>
2019-10-16 11:07:29 +0100

    Updates

---
+++ precache-manifest.6abeb8d84c6180b14c7f6cfb818c42b1.js
@@ -0,0 +1,230 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e512fdab24b93afb2a047bfadc94152",
+    "url": "/Data-Research-Platform-Front-end/index.html"
+  },
+  {
+    "revision": "5c45cc5008d479892ba9",
+    "url": "/Data-Research-Platform-Front-end/static/css/main.61bba750.chunk.css"
+  },
+  {
+    "revision": "778dcc637a5048e3a2b2",
+    "url": "/Data-Research-Platform-Front-end/static/js/2.eaefa78b.chunk.js"
+  },
+  {
+    "revision": "5c45cc5008d479892ba9",
+    "url": "/Data-Research-Platform-Front-end/static/js/main.7e99ff1d.chunk.js"
+  },
+  {
+    "revision": "ac248040fe55b0df8455",
+    "url": "/Data-Research-Platform-Front-end/static/js/runtime-main.72fcb8cc.js"
+  },
+  {
+    "revision": "73476783ca0f7583198978de7073815b",
+    "url": "/Data-Research-Platform-Front-end/static/media/0.73476783.jpg"
+  },
+  {
+    "revision": "ed986061ad9bf89c081724362956b134",
+    "url": "/Data-Research-Platform-Front-end/static/media/1.ed986061.jpeg"
+  },
+  {
+    "revision": "fe349e8528c79623ea6795ddd110985a",
+    "url": "/Data-Research-Platform-Front-end/static/media/10.fe349e85.jpeg"
+  },
+  {
+    "revision": "780ccb41c83dad3cbd24a9bcd3262586",
+    "url": "/Data-Research-Platform-Front-end/static/media/2.780ccb41.jpeg"
+  },
+  {
+    "revision": "41bf3241965cc7c2990174ed9d6e9714",
+    "url": "/Data-Research-Platform-Front-end/static/media/3.41bf3241.jpeg"
+  },
+  {
+    "revision": "026cb7af3133d721efff2a0134f7e16f",
+    "url": "/Data-Research-Platform-Front-end/static/media/4.026cb7af.jpeg"
+  },
+  {
+    "revision": "6e846951d7a04c931a0f9efa94a1316f",
+    "url": "/Data-Research-Platform-Front-end/static/media/5.6e846951.jpeg"
+  },
+  {
+    "revision": "47789d678cfff97c48fda4de3f3621d5",
+    "url": "/Data-Research-Platform-Front-end/static/media/6.47789d67.jpeg"
+  },
+  {
+    "revision": "2835396c930c8bf53ffd3beeedba7c41",
+    "url": "/Data-Research-Platform-Front-end/static/media/7.2835396c.jpeg"
+  },
+  {
+    "revision": "10b775cc930b6986ec69eb9a4adef56a",
+    "url": "/Data-Research-Platform-Front-end/static/media/8.10b775cc.jpeg"
+  },
+  {
+    "revision": "565ad4a387d2090e5d73f0c7b8de2793",
+    "url": "/Data-Research-Platform-Front-end/static/media/9.565ad4a3.jpeg"
+  },
+  {
+    "revision": "275a2629582aa2af3469364c6748cf38",
+    "url": "/Data-Research-Platform-Front-end/static/media/argon-react-white.275a2629.png"
+  },
+  {
+    "revision": "f38ddea9dedadea03c5d43c596dac13f",
+    "url": "/Data-Research-Platform-Front-end/static/media/argon-react.f38ddea9.png"
+  },
+  {
+    "revision": "14f92eea4f9497fd3bcbd014c4113440",
+    "url": "/Data-Research-Platform-Front-end/static/media/banner1.14f92eea.jpg"
+  },
+  {
+    "revision": "dfeacadac91344f26a4fb744173c7209",
+    "url": "/Data-Research-Platform-Front-end/static/media/banner3.dfeacada.png"
+  },
+  {
+    "revision": "dfeacadac91344f26a4fb744173c7209",
+    "url": "/Data-Research-Platform-Front-end/static/media/banner_img.dfeacada.png"
+  },
+  {
+    "revision": "48461ea4e797c9774dabb4a0440d2f56",
+    "url": "/Data-Research-Platform-Front-end/static/media/fa-brands-400.48461ea4.woff2"
+  },
+  {
+    "revision": "7b464e274bc331f9a765d765359635a5",
+    "url": "/Data-Research-Platform-Front-end/static/media/fa-brands-400.7b464e27.woff"
+  },
+  {
+    "revision": "947b9537bc0fecc8130d48eb753495a1",
+    "url": "/Data-Research-Platform-Front-end/static/media/fa-brands-400.947b9537.ttf"
+  },
+  {
+    "revision": "9b6c8da3c489424e2b3e9c9fb6314b37",
+    "url": "/Data-Research-Platform-Front-end/static/media/fa-brands-400.9b6c8da3.eot"
+  },
+  {
+    "revision": "b5472631dbace30d549357ec325b9c62",
+    "url": "/Data-Research-Platform-Front-end/static/media/fa-brands-400.b5472631.svg"
+  },
+  {
+    "revision": "381af09a1366b6c2ae65eac5dd6f0588",
+    "url": "/Data-Research-Platform-Front-end/static/media/fa-regular-400.381af09a.woff"
+  },
+  {
+    "revision": "73fe7f1effbf382f499831a0a9f18626",
+    "url": "/Data-Research-Platform-Front-end/static/media/fa-regular-400.73fe7f1e.ttf"
+  },
+  {
+    "revision": "7422060ca379ee9939d3b687d072acad",
+    "url": "/Data-Research-Platform-Front-end/static/media/fa-regular-400.7422060c.eot"
+  },
+  {
+    "revision": "949a2b066ec37f5a384712fc7beaf2f1",
+    "url": "/Data-Research-Platform-Front-end/static/media/fa-regular-400.949a2b06.woff2"
+  },
+  {
+    "revision": "b5a61b229c9c92a6ac21f5b0e3c6e9f1",
+    "url": "/Data-Research-Platform-Front-end/static/media/fa-regular-400.b5a61b22.svg"
+  },
+  {
+    "revision": "0079a0ab6bec4da7d6e16f2a2e87cd71",
+    "url": "/Data-Research-Platform-Front-end/static/media/fa-solid-900.0079a0ab.ttf"
+  },
+  {
+    "revision": "14a08198ec7d1eb96d515362293fed36",
+    "url": "/Data-Research-Platform-Front-end/static/media/fa-solid-900.14a08198.woff2"
+  },
+  {
+    "revision": "38508b2e7fac045869a86a15936433f7",
+    "url": "/Data-Research-Platform-Front-end/static/media/fa-solid-900.38508b2e.svg"
+  },
+  {
+    "revision": "70e65a7d34902f2c350816ecfe2f6492",
+    "url": "/Data-Research-Platform-Front-end/static/media/fa-solid-900.70e65a7d.eot"
+  },
+  {
+    "revision": "815694de1120d6c1e9d1f0895ee81056",
+    "url": "/Data-Research-Platform-Front-end/static/media/fa-solid-900.815694de.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Data-Research-Platform-Front-end/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Data-Research-Platform-Front-end/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Data-Research-Platform-Front-end/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Data-Research-Platform-Front-end/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Data-Research-Platform-Front-end/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "87be59a1f7061fc6021876aad6fee028",
+    "url": "/Data-Research-Platform-Front-end/static/media/google.87be59a1.svg"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/Data-Research-Platform-Front-end/static/media/ill-2.594b1ee1.svg"
+  },
+  {
+    "revision": "1e7c70dc7d471d845aeed9e9bead0fe5",
+    "url": "/Data-Research-Platform-Front-end/static/media/img-1-1200x1000.1e7c70dc.jpg"
+  },
+  {
+    "revision": "0ec264785c9b6efbed2651d84105f436",
+    "url": "/Data-Research-Platform-Front-end/static/media/landing.0ec26478.jpg"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/Data-Research-Platform-Front-end/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/Data-Research-Platform-Front-end/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/Data-Research-Platform-Front-end/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/Data-Research-Platform-Front-end/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/Data-Research-Platform-Front-end/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "3b8b511819e10a0b08dbabe967b605cb",
+    "url": "/Data-Research-Platform-Front-end/static/media/profile-cover.3b8b5118.jpg"
+  },
+  {
+    "revision": "278a0d250cc46de3a4f65a0791479e71",
+    "url": "/Data-Research-Platform-Front-end/static/media/profile.278a0d25.jpg"
+  },
+  {
+    "revision": "75f7ab18f707114d817522df8dea0a3b",
+    "url": "/Data-Research-Platform-Front-end/static/media/promo-1.75f7ab18.png"
+  },
+  {
+    "revision": "53033970a416368da35794389680266f",
+    "url": "/Data-Research-Platform-Front-end/static/media/team-1-800x800.53033970.jpg"
+  },
+  {
+    "revision": "dcfcf3b77210fe85b0abc8260e6fa70e",
+    "url": "/Data-Research-Platform-Front-end/static/media/team-2-800x800.dcfcf3b7.jpg"
+  },
+  {
+    "revision": "497bb3590e24c9f8b645864bfffc39b8",
+    "url": "/Data-Research-Platform-Front-end/static/media/team-3-800x800.497bb359.jpg"
+  },
+  {
+    "revision": "230071328b705f8686cabd26a85ed6a5",
+    "url": "/Data-Research-Platform-Front-end/static/media/team-4-800x800.23007132.jpg"
+  }
+]);
